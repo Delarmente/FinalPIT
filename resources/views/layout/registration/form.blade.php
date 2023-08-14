dbf813d07237096f9829746bc49cce1267246af3
Delarmente <138860594+Delarmente@users.noreply.github.com>
2023-08-14 12:00:49 +0800

    Add files via upload

---
+++ resources/views/layout/registration/form.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    body {
+
+        background-color: white;
+        color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+
+    form {
+        background-color: #EADBC8;
+        padding: 20px;
+        border: 1px solid black;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    .form-label {
+        color: black;
+    }
+
+    .form-control {
+        background-color: white;
+        border: 1px solid black;
+        color: black;
+        width:80%;
+        padding:10px;
+        margin-bottom: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+
+    }
+
+    .btn-primary {
+        background-color: black;
+        color: white;
+        border: none;
+    }
+    .btn-primary{
+        background-color: black;
+        color: white;
+        border: none;
+        padding: 7px 10px;
+        border-radius: 5px;
+        cursor: pointer;
+        font-weight: bold;
+        margin-right: 10px;
+        margin-top:9px;
+        text-decoration: none;
+
+    }
+</style>
+</head>
+<body>
+
+    <form>
+    <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" id="name" name="name" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" id="email" name="email" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="Describe Yourself" class="form-label">Describe Yourself</label>
+            <input type="Describe Yourself" id="Describe Yourself" name="Describe Yourself" class="form-control"required>
+        </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Upload Image</label>
+            <input type="file" id="image" name="image" accept="image/*" class="form-control">
+        </div>
+        <div class="button-container">
+        <button type="submit" class="btn btn-primary">Save</button>
+        <a href="index.php" class="btn btn-primary">Back to Main Page</a>
+
+        </div>
+
+
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
